ba8778339a5813f11231da9282c8640d6e45f76d
Thor-crypto <florian.thor007@gmail.com>
2026-05-04 16:26:49 +0200

    Add files via upload

---
+++ idos-discord.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg1250\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww38200\viewh21600\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
+
+\f0\fs26 \cf0 const WEBHOOK_URL = "SEM_VLOZ_WEBHOOK";\
+\
+fetch(WEBHOOK_URL, \{\
+  method: "POST",\
+  headers: \{ "Content-Type": "application/json" \},\
+  body: JSON.stringify(\{ content: "\uc0\u55357 \u56966  Funguje to!" \})\
+\});const WEBHOOK_URL = "{\field{\*\fldinst{HYPERLINK "https://discord.com/api/webhooks/1500855271478661200/Xfeiiu1LnnMjdck4uaaBmPX617z5c_-K-t6gujiR09mYGUNYvr12WvFZzeJFLWKTzF5F"}}{\fldrslt https://discord.com/api/webhooks/1500855271478661200/Xfeiiu1LnnMjdck4uaaBmPX617z5c_-K-t6gujiR09mYGUNYvr12WvFZzeJFLWKTzF5F}}";}
